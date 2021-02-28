9113f08a1d843db82e6feefeca71764dba1fda72
Jian <jian.kootker@gmail.com>
2021-03-01 00:16:02 +0100

    charts toegevoegd
    alleen nog responsive maken

--- scripts/script.js
+++ scripts/script.js
@@ -1,38 +1,99 @@
+var brandstof = document.getElementById('brandstof-chart');
+var GForce = document.getElementById('G-force-chart');
+var voorraad = document.getElementById('voorraad-chart');
+var afstandTijd = document.getElementById('afstand-tijd-chart');
 
-var ctx = document.getElementById('myChart');
-var myChart = new Chart(ctx, {
-    type: 'bar',
+
+var afstandTijdChart = new Chart(afstandTijd, {
+    type: 'pie',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+       labels: ['Afgelegd','nog te gaan'],
+       datasets: [{
+           data: [100, 70],
+           backgroundColor: [
+               'rgb(202, 240, 248)',
+               'rgb(61, 90, 128)'
+           ]
+       }]
+    },
+    options:  {
+       title: {
+           display: true,
+           text: 'Distance',
+           fontColor: '#ffffff'
+       },
+       layout: {
+           padding: 30
+       }
+     }
+});
+
+
+var brandstofChart = new Chart(brandstof, {
+    type: 'doughnut',
+    data: {
+        labels: ['Full','Empty'],
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            data: [100, 20],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
+                'rgb(255, 190, 15)',
+                'rgb(250, 30, 14)'
+            ]
         }]
     },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
+    options:  {
+        title: {
+            display: true,
+            text: 'Fuel',
+            fontColor: '#ffffff'
+        },
+        layout: {
+            padding: 30
         }
     }
 });
+
+
+var voorraadChart = new Chart(voorraad, {
+    data: {
+        datasets: [{
+            data: [80, 80, 80]
+        }],
+        labels: [
+            'oxygen',
+            'food',
+            'water'
+        ],  
+        fill: true,
+        backgroundColor: [
+            'rgb(229, 229, 229)', 
+            'rgb(252, 163, 17)', 
+            'rgb(0, 150, 199)'
+        ],
+          
+    },
+    type: 'polarArea',
+    options:  {
+        title: {
+            display: true,
+            text: 'Stockpile',
+            fontColor: '#ffffff'
+        },
+        layout: {
+            padding: 10
+        },
+            scale: {
+                angleLines: {
+                    display: true
+                },
+                ticks: {
+                    suggestedMin: 50,
+                    suggestedMax: 100
+                }
+            }
+      }
+
+
+    
+});
+
